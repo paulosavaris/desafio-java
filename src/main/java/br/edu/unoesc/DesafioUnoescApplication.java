0284155a090af479580d88d8fff07b5a01cda6a0
Natan Luiz Cavasin <natan.cavasin@unoesc.edu.br>
2024-07-16 10:34:54 -0300

    Projeto base

---
+++ src/main/java/br/edu/unoesc/DesafioUnoescApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUnoescApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUnoescApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/unoesc/DesafioUnoescApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUnoescApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
