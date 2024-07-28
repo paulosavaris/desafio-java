73ce6b109bd116edbabc9a4ce2e9bc5fc847ac46
paulosavaris <pauloosavaris@gmail.com>
2024-07-28 12:17:39 -0300

    Criado tela de Login Criado css para a tela de Login Criado Controller para a tela de Login Criado Websecurity para autenticacao para login

---
+++ src/main/java/br/edu/unoesc/security/WebSecurityConfig.java
@@ -0,0 +1,65 @@
+package br.edu.unoesc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    
+    @SuppressWarnings("removal")
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf
+                        .disable())
+                .authorizeHttpRequests()
+                .requestMatchers("/css/**", "/scripts/**").permitAll() //permissao para styles e scripts
+                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .failureUrl("/login?error=true")
+                        .defaultSuccessUrl("/", true) //a definir pagina de redirecionamento
+                        .permitAll())
+                .logout((logout) -> logout.permitAll())
+                .sessionManagement()
+                .maximumSessions(1) // Define o número máximo de sessões para um usuário
+                .expiredUrl("/login?expired=true")
+                .maxSessionsPreventsLogin(true)
+                .and()
+                .sessionFixation().migrateSession()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .invalidSessionUrl("/login");
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("paulo.savaris")
+                .password(encoder.encode("admin"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
+}
